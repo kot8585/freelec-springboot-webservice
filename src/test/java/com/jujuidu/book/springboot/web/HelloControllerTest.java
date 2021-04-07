40084d1ccfc7df6b5e50b12a6f89515a8d58b424
kot8585 <ansgywjd321@gmail.com>
2021-04-07 23:18:02 +0900

    chap05 완성

--- src/test/java/com/jujuidu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jujuidu/book/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.jujuidu.book.springboot.web;
 
+import com.jujuidu.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,13 +20,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 //테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킵니다.
 //스프링부트 테스트와 JUnit 사이에 연결자 역할을 합니다.
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+    excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+    })
 public class HelloControllerTest {
+
     @Autowired //스프링이 관리하는 빈을 주입 받습니다.
     private MockMvc mvc;
     //웹 API를 테스트할 때 사용합니다. //스프링 MVC 테스트의 시작접입니다
     // 이 클래스를 통해 HTTP GET, POST 등에 대한 API태스트 가능
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -32,6 +41,7 @@ public class HelloControllerTest {
             .andExpect(content().string("hello")); //응답 본문의 내용 검증
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto가_리턴된다() throws Exception{
         String name = "hello";
--- src/test/java/com/jujuidu/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/jujuidu/book/springboot/web/PostsApiControllerTest.java
@@ -1,5 +1,6 @@
 package com.jujuidu.book.springboot.web;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jujuidu.book.springboot.domain.posts.Posts;
 import com.jujuidu.book.springboot.domain.posts.PostsRepository;
@@ -14,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -22,6 +24,7 @@ import org.springframework.web.context.WebApplicationContext;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -40,12 +43,26 @@ public class PostsApiControllerTest {
     @Autowired
     private PostsRepository postsRepository;
 
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before //매번 테스트가 시작되기 전에 MockMvc 인스턴스를 생성성
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
     @After
     public void tearDown() throws Exception {
         postsRepository.deleteAll();
     }
 
     @Test
+    @WithMockUser(roles="USER") //인증된 모의 사용자를 만들어서 사용
     public void Posts_등록된다() throws Exception {
         //given
         String title = "title";
@@ -60,11 +77,10 @@ public class PostsApiControllerTest {
         String url = "http://localhost:" + port + "/api/v1/posts";
 
         //when
-        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
-
-        //then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+        mvc.perform(post(url) //생성된 MockMvc를 통해 API를 테스트
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
 
         //then
         List<Posts> all = postsRepository.findAll();
@@ -73,7 +89,8 @@ public class PostsApiControllerTest {
     }
 
     @Test
-    public void Posts_수정된다() {
+    @WithMockUser(roles="USER")
+    public void Posts_수정된다() throws Exception {
         //id값을 어떻게 설정하지?
      Posts savePosts = postsRepository.save(Posts.builder()
                 .title("title")
@@ -92,17 +109,15 @@ public class PostsApiControllerTest {
 
      String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
 
-     HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+    //when
+    mvc.perform(put(url)
+            .contentType(MediaType.APPLICATION_JSON_UTF8)
+            .content(new ObjectMapper().writeValueAsString(requestDto)))
+            .andExpect(status().isOk());
 
-     //when
-        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT,
-                requestEntity, Long.class);
-
-     //then
-     assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-     assertThat(responseEntity.getBody()).isGreaterThan(0L);
-     List<Posts> all = postsRepository.findAll();
-     assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
-     assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
+    //then
+    List<Posts> all = postsRepository.findAll();
+    assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+    assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
     }
 }
